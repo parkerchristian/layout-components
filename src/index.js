332ac5c6df1b78b925f19f6c53a8082765f72520
Parker Helland <hellandp@gmail.com>
2019-05-13 13:16:17 -0700

    done with master

--- src/index.js
+++ src/index.js
@@ -1,7 +1,31 @@
-console.log('hibye');
+// append a ul
+// append li w/ 'spot', 'rover', 'bingo', and 'joe'
+import styles from './index.css';
 
-// function funky() {
-//   return 2;
-// }
+const root = document.getElementById('root');
+const ul = document.createElement('ul');
+ul.className = styles.dogs;
+root.appendChild(ul);
 
-// funky();
+['spot', 'rover', 'bingo', 'joe']
+  .forEach(name => {
+    const li = document.createElement('li');
+    li.textContent = name;
+    ul.appendChild(li);
+  });
+
+// const li = document.createElement('li');
+// const a = document.createElement('li');
+// const b = document.createElement('li');
+// const c = document.createElement('li');
+
+// li.textContent = 'spot';
+// a.textContent = 'rover';
+// b.textContent = 'bingo';
+// c.textContent = 'joe';
+
+// ul.appendChild(li).style.color = 'blue';
+// ul.appendChild(a);
+// ul.appendChild(b);
+// ul.appendChild(c);
+// root.appendChild(ul);
