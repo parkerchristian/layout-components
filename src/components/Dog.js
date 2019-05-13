652fac2476c9639749f29e0f09cf1d40da321fd4
Parker Helland <hellandp@gmail.com>
2019-05-13 14:50:43 -0700

    completed simple dog exercise

---
+++ src/components/Dog.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default function Dog() {
+  return (
+    <dl>
+      <dt>Name</dt>
+      <dd>Jimmy</dd>
+
+      <dt>Age</dt>
+      <dd>1</dd>
+
+      <dt>Weight</dt>
+      <dd>100 lbs</dd>
+    </dl>
+  );
+}
